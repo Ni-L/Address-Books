8c83b8e36fababc2a636c8123dfafaddce3e19a5
unknown <nilimawadal1@gmail.com>
2021-04-24 09:56:40 +0530

    [Nilima] Adding Contact

---
+++ Address-Book/Address-Book/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{/// <summary>
+/// Adding Contact Details
+/// </summary>
+    class Person
+    {
+        //Crete Variables
+        string firstName;
+        string lastName;
+        string address;
+        string city;
+        string state;
+        string zip;
+        string phoneNumber;
+        string email;
+
+        //Adding Method
+        public Person(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            //Instance Mathod
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        //Adding get Method
+        public string getName()
+        {
+            return this.firstName;
+        }
+        public string getlastName()
+        {
+            return this.lastName;
+        }
+        public string getaddress()
+        {
+            return this.address;
+        }
+        public string getcity()
+        {
+            return this.city;
+        }
+        public string getstate()
+        {
+            return this.state;
+        }
+        public string getzip()
+        {
+            return this.zip;
+        }
+        public string getphone()
+        {
+            return this.phoneNumber;
+        }
+        public string getemail()
+        {
+            return this.email;
+        }
+        //Adding  Method to storing Data
+        public string Tostring()
+        {
+            return "First Name:" + firstName + "\nLast Name:" + lastName + "\nAddress:" + address + "\nState:" + state + "\nCity:" + city + "\nZip:" + zip + "\nPhoneNumber:" + phoneNumber;
+            // return "Name is:" + this.firstName +\n+ this.lastName+
+        }
+
+    }
+}
--- Address-Book/Address-Book/Program.cs
+++ Address-Book/Address-Book/Program.cs
@@ -7,10 +7,28 @@ using System.Threading.Tasks;
 namespace Address_Book
 {
     class Program
-    {
+    {/// <summary>
+    /// Main Mathod
+    /// </summary>
         static void Main(string[] args)
         {
             Console.WriteLine("******WelCome To Address Book******");
+            //Display Contact
+            Person person = new Person("Naina", "Wadal", "akola", "pune", "maharashtra", "444101", "7363353435", "nilimawadal1@gmail.com");
+            Console.WriteLine(person.getName());
+            Console.WriteLine(person.getlastName());
+            Console.WriteLine(person.getaddress());
+            Console.WriteLine(person.getcity());
+            Console.WriteLine(person.getstate());
+            Console.WriteLine(person.getzip());
+            Console.WriteLine(person.getphone());
+            Console.WriteLine(person.getemail());
+            //contacts.FirstName = Naina
+            Console.Read();
         }
+
+
+
     }
 }
+        
