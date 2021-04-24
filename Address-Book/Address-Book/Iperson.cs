dd88b258ee2240570da7b50186edff6f13f39fa6
unknown <nilimawadal1@gmail.com>
2021-04-24 10:33:22 +0530

    [Nilima]Edit contact by Person Name

---
+++ Address-Book/Address-Book/Iperson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{/// <summary>
+/// Create Interface
+/// </summary>
+    interface IPerson
+    {
+        void AddContact(string firstName, string lastName, string address, string city, string state, int email, int zip, long phoneNumber);
+        void EditContact(string name);
+    }
+}
--- Address-Book/Address-Book/Program.cs
+++ Address-Book/Address-Book/Program.cs
@@ -7,10 +7,35 @@ using System.Threading.Tasks;
 namespace Address_Book
 {
     class Program
-    {
+    {/// <summary>
+    /// Create Main Method
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("******WelCome To Address Book******");
+            AddressBook addressBook = new AddressBook(); //object of AddessBook class
+            Console.WriteLine("Enter First Name :");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Email :");
+            int email = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Zip :");
+            int zip = Convert.ToInt32(Console.ReadLine());//conver into int using ToInt32()
+            Console.WriteLine("Enter Phone Number :");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
+            addressBook.ViewContact();//calling method
+            addressBook.EditContact(firstName);
+            addressBook.ViewContact();//calling method
+            Console.Read();
         }
     }
 }
