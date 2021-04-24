b13ec7bdc79d12ab3af8938b586084f4ed1fc492
unknown <nilimawadal1@gmail.com>
2021-04-24 10:45:06 +0530

    [Nilima] Added Deleting Contact

---
+++ Address-Book/Address-Book/IPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{/// <summary>
+ /// signature of interface
+ /// </summary>
+    interface IPerson
+    {
+        void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+        void EditContact(string name);
+        void DeleteContact(string name);
+        // void ViewContact();
+    }
+}
--- Address-Book/Address-Book/Program.cs
+++ Address-Book/Address-Book/Program.cs
@@ -5,12 +5,62 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Address_Book
-{
+{/// <summary>
+/// Main Method
+/// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("******WelCome To Address Book******");
+            AddressBook addressBook = new AddressBook(); //object of AddessBook class
+
+            int Choice = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter First Name :");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name :");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Enter Address :");
+                string address = Console.ReadLine();
+                Console.WriteLine("Enter City :");
+                string city = Console.ReadLine();
+                Console.WriteLine("Enter State :");
+                string state = Console.ReadLine();
+                Console.WriteLine("Enter Email :");
+                string email = Console.ReadLine();
+                Console.WriteLine("Enter Zip :");
+                int zip = Convert.ToInt32(Console.ReadLine());//conver into int using ToInt32()
+                Console.WriteLine("Enter Phone Number :");
+                long phoneNumber = Convert.ToInt64(Console.ReadLine());
+                addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
+
+
+                switch (Choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First Name Of Contact To Edit :");
+                        string nameToEdit = Console.ReadLine();
+                        addressBook.EditContact(nameToEdit);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter First Name Of Contact To Delete :");
+                        string nameToDelete = Console.ReadLine();
+                        addressBook.DeleteContact(nameToDelete);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter First Name Of Contact To View :");
+                        string nameToView = Console.ReadLine();
+                        addressBook.ViewContact();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("It is Not Found Any Information.");
+                        break;
+                }
+            } while (Choice != 4);
         }
     }
+
 }
